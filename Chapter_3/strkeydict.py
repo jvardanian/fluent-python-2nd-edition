3b3f6f0850196d13432e5df738fa89ff10eea31b
John Vardanian <jvardanian@gmail.com>
2022-05-11 16:11:01 -0700

    example subclass of UserDict

---
+++ Chapter_3/strkeydict.py
@@ -0,0 +1,19 @@
+"""
+strkeydict.py
+StrKeyDict always converts non-string keys to `str`
+"""
+import collections
+
+
+class StrKeyDict(collections.UserDict):
+
+    def __missing__(self, key):
+        if isinstance(key, str):
+            raise KeyError(key)
+        return self[str(key)]
+
+    def __contains__(self, key):
+        return str(key) in self.data
+
+    def __setitem__(self, key, item):
+        self.data[str(key)] = item

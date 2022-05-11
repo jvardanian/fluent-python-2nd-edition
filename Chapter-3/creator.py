8679acb822d59070f72d0a2867d92ca72d6faf3e
John Vardanian <jvardanian@gmail.com>
2022-05-11 12:20:14 -0700

    add pattern matching with mappings example

---
+++ Chapter-3/creator.py
@@ -0,0 +1,33 @@
+"""
+creator.py
+Pattern matching with mapping—requires Python ≥ 3.10
+"""
+from collections import OrderedDict
+
+
+def get_creators(record: dict) -> list:
+    match record:
+        case {'type': 'book', 'api': 2, 'authors': [*names]}:
+            return names
+        case {'type': 'book', 'api': 1, 'author': name}:
+            return [name]
+        case {'type': 'book'}:
+            raise ValueError(f"Invalid 'book' record: {record!r}")
+        case {'type': 'movie', 'director': name}:
+            return [name]
+        case _:
+            raise ValueError(f'Invalid record: {record!r}')
+
+
+def main():
+    b1 = dict(api=1, author='Douglas Hofstadter',
+              type='book', title='Gödel, Escher, Bach')
+    print(get_creators(b1))
+
+    b2 = OrderedDict(api=2, type='book', title='Python in a Nutshell',
+                     authors='Martelli Ravenscroft Holden'.split())
+    print(get_creators(b2))
+
+
+if __name__ == '__main__':
+    main()

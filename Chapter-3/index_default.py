535db52c53ad5758f426544afe6eb3d5759ec1ce
John Vardanian <jvardanian@gmail.com>
2022-05-11 13:08:22 -0700

    add example of collections.defaultdict; update documentation for setdefault example (index.py)

--- Chapter-3/index_default.py
+++ Chapter-3/index_default.py
@@ -1,22 +1,23 @@
 """
-index.py
+index_default.py
 Build and display an index mapping word -> list of occurrences
-Example to demonstrate usage of dict.setdefault
+Example to demonstrate usage of collections.defaultdict
 """
 
+import collections
 import re
 import sys
 
 WORD_RE = re.compile(r'\w+')
 
-index = {}
+index = collections.defaultdict(list)
 with open(sys.argv[1], encoding='utf-8') as fp:
     for line_no, line in enumerate(fp, 1):
         for match in WORD_RE.finditer(line):
             word = match.group()
             column_no = match.start() + 1
             location = (line_no, column_no)
-            index.setdefault(word, []).append(location)
+            index[word].append(location)
 
 # display in alphabetical order
 for word in sorted(index, key=str.upper):
